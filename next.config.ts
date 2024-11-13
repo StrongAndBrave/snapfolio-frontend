add2022317654e3dfaddbea4ab13ada20ec97b77
Margaret <vinolin.bright@gmail.com>
2024-11-13 08:42:58 +0100

    feat: reset styles, create global styles, add scripts for prettier and stylelint, update eslint version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    sassOptions: {
+        additionalData: `@import "./src/app/styles/variables.scss";`,
+    },
 };
 
 export default nextConfig;
