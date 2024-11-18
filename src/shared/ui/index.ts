16608d29e60cc135686441532236eb0d5d82f4a7
Денис Касперович <kasperovich.deonis@gmail.com>
2024-11-18 12:21:15 +0300

    INC-7 refactor: added an index

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './button';
+export { Select } from './select';
+export { Tab } from './tab';
+export { Textarea } from './textarea';
+export { Checkbox } from './checkbox';
+export { RadioButton } from './radiobutton';
