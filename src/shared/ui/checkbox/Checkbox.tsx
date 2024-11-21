4469161dc7d9b0bb7f2de8f77a948f8f5d855b3f
Денис Касперович <kasperovich.deonis@gmail.com>
2024-11-21 22:54:36 +0300

    INC-23 refactor: change styles checkbox

--- src/shared/ui/checkbox/Checkbox.tsx
+++ src/shared/ui/checkbox/Checkbox.tsx
@@ -1,38 +1,37 @@
-import React, { ComponentPropsWithRef, ChangeEvent } from 'react';
+import React, {ComponentPropsWithRef, ChangeEvent} from 'react';
 import styles from './Checkbox.module.scss';
 
 type Props = {
     checked: boolean;
     onChange: (checked: boolean) => void;
     children?: React.ReactNode;
+    className?: string;
 } & ComponentPropsWithRef<'input'>;
 
-export const Checkbox = ({ checked, onChange, children, disabled, ...rest }: Props) => {
+export const Checkbox = ({checked, onChange, children, disabled, className, ...rest}: Props) => {
     const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (!disabled) {
-            onChange(e.currentTarget.checked);
+            onChange ? onChange(e.currentTarget.checked) : '';
         }
     };
 
-    const handleResetFocus = (e: React.MouseEvent) => {
-        (e.currentTarget as HTMLElement).blur();
-    };
-
     return (
-        <label className={`${styles.wrapper} ${disabled ? styles.disabled : ''}`}>
-            <input
-                type="checkbox"
-                checked={checked}
-                onChange={handleCheckboxChange}
-                disabled={disabled}
-                className={styles.hiddenCheckbox}
-                {...rest}
-            />
-            <div
-                className={`${styles.checkbox} ${checked ? styles.checked : ''}`}
-                tabIndex={0}
-                onClick={handleResetFocus}
-            />
+        <label className={`${styles.wrapper} ${className} ${disabled ? styles.disabled : ''}`}>
+            <div className={styles.outline}>
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={handleCheckboxChange}
+                    disabled={disabled}
+                    className={styles.hiddenCheckbox}
+                    {...rest}
+                />
+                <span className={styles.outlineBg} aria-hidden>outline</span>
+                <div
+                    className={`${styles.checkbox} ${checked ? styles.checked : ''}`}
+                    aria-hidden
+                />
+            </div>
             {children && <span className={styles.label}>{children}</span>}
         </label>
     );
