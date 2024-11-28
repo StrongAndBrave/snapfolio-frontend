e2ba90122f66d70d512f9155ca6617c5ee195f2f
Денис Касперович <kasperovich.deonis@gmail.com>
2024-11-28 22:03:16 +0300

    INC-18 refactor: change shared components

--- src/shared/ui/checkbox/Checkbox.tsx
+++ src/shared/ui/checkbox/Checkbox.tsx
@@ -1,38 +1,16 @@
-import React, {ComponentPropsWithRef, ChangeEvent} from 'react';
+import React, { ComponentPropsWithRef } from 'react';
 import styles from './Checkbox.module.scss';
 
 type Props = {
-    checked: boolean;
-    onChange: (checked: boolean) => void;
     children?: React.ReactNode;
-    className?: string;
-} & ComponentPropsWithRef<'input'>;
+} & Omit<ComponentPropsWithRef<'input'>, 'value'>;
 
-export const Checkbox = ({checked, onChange, children, disabled, className, ...rest}: Props) => {
-    const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (!disabled) {
-            onChange ? onChange(e.currentTarget.checked) : '';
-        }
-    };
-
+export const Checkbox = ({ children, ...rest }: Props) => {
     return (
-        <label className={`${styles.wrapper} ${className} ${disabled ? styles.disabled : ''}`}>
-            <div className={styles.outline}>
-                <input
-                    type="checkbox"
-                    checked={checked}
-                    onChange={handleCheckboxChange}
-                    disabled={disabled}
-                    className={styles.hiddenCheckbox}
-                    {...rest}
-                />
-                <span className={styles.outlineBg} aria-hidden>outline</span>
-                <div
-                    className={`${styles.checkbox} ${checked ? styles.checked : ''}`}
-                    aria-hidden
-                />
-            </div>
-            {children && <span className={styles.label}>{children}</span>}
+        <label className={styles.label}>
+            <input type="checkbox" className={styles.input} {...rest} />
+            <span className={styles.fake}></span>
+            {children && <span className={styles.text}>{children}</span>}
         </label>
     );
 };
