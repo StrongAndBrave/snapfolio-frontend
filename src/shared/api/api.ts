59ce23dcb91908559994ebf0b6c8e73f67d4ae29
Yana Temirgalieva <kolesiko.yana@gmail.com>
2024-12-10 15:44:49 +0300

    feat: add update-tokens logic

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,7 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const baseUrl = 'https://inctagram.work/api/v1/';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import customBaseQuery from './customBaseQuery';
 
 export const baseApi = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl }),
+    baseQuery: customBaseQuery,
     endpoints: () => ({}),
 });
---
+++ src/shared/api/customBaseQuery.ts
@@ -0,0 +1,33 @@
+import { BaseQueryFn, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
+
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://inctagram.work/api/v1/' });
+
+type TokensResponse = {
+    accessToken: string;
+};
+
+const customBaseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+    args,
+    api,
+    extraOptions,
+) => {
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result.error && result.error.status === 401) {
+        // Попытка обновить токен
+        const refreshResult = await baseQuery('/auth/update-tokens', api, extraOptions);
+
+        if (refreshResult.data) {
+            // Сохранить новый токен и повторить исходный запрос
+            localStorage.setItem('accessToken', (refreshResult.data as TokensResponse).accessToken);
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            // Обработка ошибки обновления токена
+            api.dispatch({ type: '/auth/logout' });
+        }
+    }
+
+    return result;
+};
+
+export default customBaseQuery;
