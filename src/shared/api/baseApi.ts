a890086fbc89952756ad4dd6f0c7d74bb89174e5
Денис Касперович <kasperovich.deonis@gmail.com>
2024-12-14 11:01:49 +0300

    INC-18 feat: request interception (token update)

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,23 +1,9 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import {RootState} from "@/app/store/store";
+import { createApi} from '@reduxjs/toolkit/query/react';
+import {baseQueryWithReauth} from "@/shared/api/baseQueryWithReauth";
 
 export const baseApi = createApi({
     reducerPath: 'inctagramApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.NEXT_PUBLIC_API_URL,
-        prepareHeaders: (headers, { getState }) => {
-            const state = getState() as RootState;
-            const accessToken = state.auth?.accessToken;
-
-            if (accessToken) {
-                headers.set('Authorization', `Bearer ${accessToken}`);
-            }
-
-            headers.set('Content-Type', 'application/json');
-            return headers;
-        },
-        credentials: 'include',
-    }),
+    baseQuery: baseQueryWithReauth,
     endpoints: () => ({}),
     tagTypes: [],
 });
