0028e42e1da1b55985e6595dbb532a4988c9a5c4
Rustem <slehder5657@gmail.com>
2024-12-25 16:26:29 +0500

    feat: add types to api

--- src/features/posts/api/postsApi.ts
+++ src/features/posts/api/postsApi.ts
@@ -2,7 +2,7 @@ import { baseApi } from '@/shared/api/baseApi';
 
 export const postsApi = baseApi.injectEndpoints({
     endpoints: builder => ({
-        getPosts: builder.query<any, any>({
+        getPosts: builder.query<ResponseData, GetPostsQueryParams>({
             query: ({ userName, pageSize, pageNumber, sortBy, sortDirection }) => ({
                 url: `/api/v1/${userName}`,
                 params: { pageSize, pageNumber, sortBy, sortDirection },
---
+++ src/features/posts/api/types.ts
@@ -0,0 +1,44 @@
+type Image = {
+    url: string;
+    width: number;
+    height: number;
+    fileSize: number;
+    createdAt: string; // ISO 8601 format
+    uploadId: string;
+};
+
+type Owner = {
+    firstName: string;
+    lastName: string;
+};
+
+type Item = {
+    id: number;
+    userName: string;
+    description: string;
+    location: string;
+    images: Image[];
+    createdAt: string;
+    updatedAt: string;
+    ownerId: number;
+    avatarOwner: string;
+    owner: Owner;
+    likesCount: number;
+    isLiked: boolean;
+    avatarWhoLikes: boolean;
+};
+
+type ResponseData = {
+    pageSize: number;
+    totalCount: number;
+    notReadCount: number;
+    items: Item[];
+};
+
+interface GetPostsQueryParams {
+    userName: string;
+    pageSize: number;
+    pageNumber: number;
+    sortBy: string;
+    sortDirection: 'asc' | 'desc';
+}
