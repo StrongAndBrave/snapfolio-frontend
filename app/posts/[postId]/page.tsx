1baf7d14f553ef56b94924d32ae0f1e0c9acd345
Денис Касперович <kasperovich.deonis@gmail.com>
2025-04-14 18:19:00 +0300

    refactor: refactor posts type

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,9 @@
 import { PostRedirect } from '@/features/posts/ui/view-post/PostRedirect';
 
-export default function PostPage({ params }: { params: { postId: string } }) {
+type Props = {
+    params: { postId: string }
+}
+
+export default function PostPage({ params }: Props) {
     return <PostRedirect postId={Number(params.postId)} />;
 }
